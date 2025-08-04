e6219c3ab4e69643fd44510b046468058e601596
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-08-04 18:43:52 +0530

    add new programs

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,21 +1,20 @@
+#include <climits>
 #include <iostream>
-#include <unordered_map>
 #include <vector>
 using namespace std;
 
 int main() {
-  vector<int> vec = {1, 2, 2, 2, 1, 1};
-  unordered_map<int, int> mapContainer;
-
-  for (int i : vec) {
-    mapContainer[i]++;
-  }
-
-  for (auto i : mapContainer) {
-    if (i.second > (vec.size() / 2)) {
-      cout << i.first << " = " << i.second;
+  vector<int> heightArr = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+  int maxWater = 0;
+  for (int i = 0; i < heightArr.size(); i++) {
+    for (int j = i + 1; j < heightArr.size(); j++) {
+      int gap = j - i;
+      int minHeight = min(heightArr[i], heightArr[j]);
+      int currentWater = minHeight * gap;
+      maxWater = max(currentWater, maxWater);
     }
   }
 
+  cout << maxWater;
   return 0;
 }
