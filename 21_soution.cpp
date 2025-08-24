e6e5f56e6626adf18758012565aaa02cf23350e3
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-08-24 12:23:59 +0530

    solve Q21. Remove All Occurrences of a Substring

---
+++ 21_soution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str = "daabcbaabcbc";
+  string part = "abc";
+  while (str.find(part) <= str.length()) {
+    int idx = str.find(part);
+    str.erase(idx, part.length());
+  }
+  cout << str;
+  return 0;
+}
